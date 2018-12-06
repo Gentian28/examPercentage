8c7c8d7f9a8739fbebed54d96665e8edb29024f7
gentian_shkembi <gentian.shkembi@gmail.com>
2018-12-06 09:24:25 +0100

    styled result card

--- js/services/get.js
+++ js/services/get.js
@@ -13,17 +13,21 @@ export function getResults(user) {
                 <article class="card">
                     <h3 class="cardHeader">Exam Name: ${data.exam}</h3>
                     <section class="cardContent">
-                        <p>Total Questions: ${totalQuestions}</p>
-                        <p>Correct Answers: ${data.correctAnswers}</p>
-                        <p>Wrong Answers: ${data.wrongAnswers}</p>
-                        <p>Wrong Answers List: ${data.wrongAnswersNumber}</p>
-                        <p>Problematic Questions: ${data.problematicQuestions}</p>
-                        <p>Date: ${data.date.toDate()}</p>
-                        <p>Version: ${data.version}</p>
-                        <p>Attempt: ${data.tryNr}</p>
-                    </section>
-                    <p>Percentage: ${percentage.toString().substring(0, 5)}%</p>
-                </article>
+                        <div><span class="cardFieldLabel">Total Questions:</span> ${totalQuestions}</div>
+                        <div><span class="cardFieldLabel">Correct Answers:</span> ${data.correctAnswers}</div>
+                        <div><span class="cardFieldLabel">Wrong Answers:</span> ${data.wrongAnswers}</div>
+                        <div><span class="cardFieldLabel">Wrong Answers List:</span> ${data.wrongAnswersNumber}</div>
+                        <div><span class="cardFieldLabel">Problematic Questions:</span> ${data.problematicQuestions}</div>
+                        <div><span class="cardFieldLabel">Date:</span> ${data.date.toDate()}</div>
+                        <div><span class="cardFieldLabel">Version:</span> ${data.version}</div>
+                        <div><span class="cardFieldLabel">Attempt:</span> ${data.tryNr}</div>
+                    </section>`
+            if (percentage >= 70) {
+                resultsList += `<div class="success cardPercentage">Percentage: ${percentage.toString().substring(0, 5)}%</div>`;
+            } else {
+                resultsList += `<div class="error cardPercentage">Percentage: ${percentage.toString().substring(0, 5)}%</div>`;
+            }
+            resultsList += `</article>
             </li>`;
         });
 
