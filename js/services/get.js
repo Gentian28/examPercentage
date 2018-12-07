ab956f596ac51b4fc9dca511a59a0758f11db0d8
gentian_shkembi <gentian.shkembi@gmail.com>
2018-12-07 16:30:06 +0100

    working with cards

--- js/services/get.js
+++ js/services/get.js
@@ -3,35 +3,50 @@ import { percentageCalculator } from '.././percentage.js';
 export function getResults(user) {
     let resultsList = "";
 
-    db.collection("examResult").where("user", "==", user).get().then((querySnapshot) => {
+    db.collection("examResult").where("user", "==", user).orderBy("date").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             let data = doc.data();
             let totalQuestions = data.correctAnswers + data.wrongAnswers;
             let percentage = percentageCalculator(totalQuestions, data.wrongAnswers);
-            console.log(percentage);
             resultsList += `<li>
                 <article class="card">
-                    <h3 class="cardHeader">Exam Name: ${data.exam}</h3>
+                    <section class="cardHeader flex space-between align-center">
+                        <h3>Exam: ${data.exam}</h3>
+                        <span class="cardDate">${data.date.toDate().toString().substring(4, 15)}</span>
+                    </section>
                     <section class="cardContent">
                         <div><span class="cardFieldLabel">Total Questions:</span> ${totalQuestions}</div>
                         <div><span class="cardFieldLabel">Correct Answers:</span> ${data.correctAnswers}</div>
                         <div><span class="cardFieldLabel">Wrong Answers:</span> ${data.wrongAnswers}</div>
-                        <div><span class="cardFieldLabel">Wrong Answers List:</span> ${data.wrongAnswersNumber}</div>
-                        <div><span class="cardFieldLabel">Problematic Questions:</span> ${data.problematicQuestions}</div>
-                        <div><span class="cardFieldLabel">Date:</span> ${data.date.toDate()}</div>
                         <div><span class="cardFieldLabel">Version:</span> ${data.version}</div>
                         <div><span class="cardFieldLabel">Attempt:</span> ${data.tryNr}</div>
-                    </section>`
+                    </section>
+                    <section class="cardList flex justify-center align-center">
+                        <div class="cardListItem">
+                            <p>Wrong Answers List</p>
+                            <div>
+                                <p class="cardHeader cardListHeader flex space-between align-center"><span>Wrong Answers List</span><img src="images/icons/close.png" alt="Close" class="closeList" title="Close"></p>
+                                <div class="cardContent">${data.wrongAnswersNumber}</div>
+                            </div>
+                        </div>
+                        <div class="cardListItem">
+                            <p>Problematic Questions</p>
+                            <div>
+                                <p class="cardHeader cardListHeader flex space-between align-center"><span>Problematic Questions</span><img src="images/icons/close.png" alt="Close" class="closeList" title="Close"></p>
+                                <div class="cardContent">${data.problematicQuestions}</div>
+                            </div>
+                        </div>
+                    </section>
+                    `
             if (percentage >= 70) {
-                resultsList += `<div class="success cardPercentage">Percentage: ${percentage.toString().substring(0, 5)}%</div>`;
+                resultsList += `<section class="success cardPercentage">Percentage: ${percentage.toString().substring(0, 5)}%</section>`;
             } else {
-                resultsList += `<div class="error cardPercentage">Percentage: ${percentage.toString().substring(0, 5)}%</div>`;
+                resultsList += `<section class="error cardPercentage">Percentage: ${percentage.toString().substring(0, 5)}%</section>`;
             }
             resultsList += `</article>
             </li>`;
         });
 
         return results.innerHTML = resultsList;
-
     });
 }
